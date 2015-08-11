271928da83214772ea9cea33d0d918fded115f7d
JonMitso <yianni152014@gmail.com>
2015-08-11 17:44:07 +1000

    add player hit with left and right shift, with score decrement for miss hits

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -37,7 +37,6 @@ namespace CardGames.GameLogic
 		public Snap ()
 		{
 			_deck = new Deck ();
-			_gameTimer = SwinGame.CreateTimer ();
 		}
 
 		/// <summary>
@@ -93,8 +92,6 @@ namespace CardGames.GameLogic
 				_deck.Shuffle ();		// Return the cards and shuffle
 
 				FlipNextCard ();		// Flip the first card...
-
-				_gameTimer.Start ();
 			}
 		}
 			
@@ -114,11 +111,7 @@ namespace CardGames.GameLogic
 		/// </summary>
 		public void Update()
 		{
-			if (_gameTimer.Ticks > _flipTime)
-			{
-				_gameTimer.Reset ();
-				FlipNextCard ();
-			}
+			//TODO: implement update to automatically slip cards!
 		}
 
 		/// <summary>
@@ -147,11 +140,14 @@ namespace CardGames.GameLogic
 				_score[player]++;
 				//TODO: consider playing a sound here...
 			}
-
+			
+else if ( player >= 0 && player < _score.Length) 
+{ 
+_score[player]--; 
+}
 
 			// stop the game...
 			_started = false;
-			_gameTimer.Stop ();
 		}
 	
 		#region Snap Game Unit Tests
